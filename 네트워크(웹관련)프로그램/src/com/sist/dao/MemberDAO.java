5e259ac2789a4db4827ceb43139eb86bad7b65b7
HiHyerin <songhr95@gmail.com>
2022-12-01 22:44:06 +0900

    221201 네트워크(웹관련)프로그램

---
+++ 네트워크(웹관련)프로그램/src/com/sist/dao/MemberDAO.java
@@ -0,0 +1,71 @@
+package com.sist.dao;
+import java.sql.*;
+import java.util.*;
+public class MemberDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private final String URL="jdbc:oracle:thin:@localhost:1521:xe";
+	
+	// 드라이버 등록
+	public MemberDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch(Exception ex) {}
+	}
+	// 연결
+	public void getConnection() {
+		try {
+			conn=DriverManager.getConnection(URL,"hr","happy");
+		}catch(Exception ex) {}
+	}
+	// 해제
+	public void disConnection() {
+		try {
+			if(ps!=null)ps.close();
+			if(conn!=null)conn.close();
+			//exit
+			
+		}catch(Exception ex) {}
+	}
+	// 기능 => 로그인
+	public String isLogin(String id,String pwd) {
+		String result="";
+		try {
+			// 연결
+			getConnection();
+			
+			// SQL 문장 제작
+			String sql="SELECT COUNT(*) FROM member WHERE id='"+id +"'";
+			ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			int count=rs.getInt(1);
+			if(count==0) {
+				result="NOID";
+				
+			}else {
+				sql="SELECT pwd,name FROM member WHERE id='"+id+"'";
+				ps=conn.prepareStatement(sql);
+				rs.next();
+				String db_pwd=rs.getString(1);
+				String name=rs.getString(2);
+				
+				if(db_pwd.equals(pwd)) {
+					result=name;
+				}else {
+					result="NOPWD";
+				}
+						
+			}
+					
+
+			// 오라클 전송
+			// 결과값 받기
+			
+		}catch(Exception ex) {ex.printStackTrace();} // 오류확인
+		finally {
+			disConnection(); //해제
+		}
+		return result;
+	}
+}
