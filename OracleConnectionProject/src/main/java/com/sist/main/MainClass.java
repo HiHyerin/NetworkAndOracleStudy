194b130aae1ed947924679b6e3cde91287e698ed
HiHyerin <116174672+HiHyerin@users.noreply.github.com>
2022-12-20 17:50:51 +0900

    221220 pl/sql정리, 데이터베이스 연동 프로그래밍

---
+++ OracleConnectionProject/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,26 @@
+package com.sist.main;
+import java.util.*;
+import com.sist.dao.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SeoulDAO dao=new SeoulDAO();
+		ArrayList<SeoulVO> list=dao.seoulListData(1);
+		System.out.println("========서울명소========");
+		for(SeoulVO vo:list) {
+			System.out.println(vo.getNo()+"."+vo.getTitle());
+		}
+		System.out.println("======서울자연&관광======");
+		list=dao.seoulListData(2);
+		for(SeoulVO vo:list) {
+			System.out.println(vo.getNo()+"."+vo.getTitle());
+		}
+		System.out.println("======서울쇼핑======");
+		list=dao.seoulListData(3);
+		for(SeoulVO vo:list) {
+			System.out.println(vo.getNo()+"."+vo.getTitle());
+		}
+	}
+
+}
